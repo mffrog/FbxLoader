0d1296b28488f0673466ba0c91cd9e270e3cfc98
Tomoya Fujii <tomoyafujii@fujiitomoya-no-MacBook-Pro.local>
2017-11-16 14:20:12 +0900

    Initial Commit

---
+++ FbxLoader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FbxLoader
+//
+//  Created by Tomoya Fujii on 2017/11/16.
+//  Copyright © 2017年 TomoyaFujii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
